560fea9caa48f824780ffd4dd6084a84b8feea05
nicolas castex <nicolas.castex.pro@gmail.com>
2024-03-26 14:57:31 +0100

    adding ways to use db python

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,24 @@
+from http.client import HTTPException
+from pydantic import BaseModel, EmailStr
 from fastapi import FastAPI
+from database import connect, initialize_db, create_user
 
 app = FastAPI()
+conn = connect()
+cursor = conn.cursor()
 
+class User(BaseModel):
+    name: str
+    age: int
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.on_event("startup")
+async def startup_event():
+    initialize_db()
 
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.post("/users/")
+async def add_user(user: User):
+    user_id = create_user(user.name, user.age)
+    if user_id:
+        return {"id": user_id, "name": user.name, "age": user.age}
+    else:
+        raise HTTPException(status_code=400, detail="User not created")
