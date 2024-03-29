bb32f3d53c892f5393a5e180d036c2e8ec01bd95
ahmed aoussar <ahmed.aoussar2000@gmail.com>
2024-03-29 16:52:29 +0100

    barre de recherche

--- frontend/src/Pages/Accueil.jsx
+++ frontend/src/Pages/Accueil.jsx
@@ -1,8 +1,12 @@
 
-export const Accueil = () => {
-    return (
-        <div>
+// pages/accueil.jsx
 
-        </div>
-    )
-}
+import SearchBar from '../component/SearchBar'; // Importez le composant de barre de recherche
+
+export const Accueil = () => {
+  return (
+    <div>
+      <SearchBar /> {/*  le composant de barre de recherche */}
+    </div>
+  );
+};
