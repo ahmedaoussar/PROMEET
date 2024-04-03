e0c436fff90d39df7b3c8d0d5bccd996679f6db4
nicolas castex <nicolas.castex.pro@gmail.com>
2024-04-03 16:23:35 +0200

    update router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,48 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import {ThemeProvider} from "@material-tailwind/react";
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import {Outlet} from "react-router-dom";
+import {Footer} from "./Composants/Footer.jsx";
+import {Accueil} from "./Pages/Accueil.jsx";
+import {BarreNavigation} from "./Composants/BarreNavigation.jsx";
+
+function Layout() {
+    return (
+        <div className={'w-10/12 md:container mx-auto'}>
+            <BarreNavigation></BarreNavigation>
+            <Outlet/>
+            <Footer/>
+        </div>
+    );
+}
+
+const router = createBrowserRouter([
+    {
+        element: <Layout/>,
+        errorElement: <div>Not Found</div>,
+        children: [
+            {
+                path: '/',
+                element: <Accueil/>
+            },
+            {
+                path: '/login',
+                element: <div></div>
+            },
+            {
+                path: '/recherche',
+                element: <div></div>
+            }
+        ]
+    }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <ThemeProvider>
-            <App/>
+            <RouterProvider router={router}/>
         </ThemeProvider>
     </React.StrictMode>,
 )
