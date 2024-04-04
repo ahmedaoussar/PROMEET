ade77caabebcc20b82834bf9e965476a018eec5b
nicolas castex <nicolas.castex.pro@gmail.com>
2024-04-04 10:30:39 +0200

    saved

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,14 @@
 from fastapi import HTTPException
 from pydantic import BaseModel, EmailStr
 from fastapi import FastAPI
-from database import connect, initialize_db, recherche_dans_la_base,findUserById
+from database import connect, initialize_db, recherche_dans_la_base, findUserById, createUser
 from src.model.User import User
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 240
 
 app = FastAPI()
 conn = connect()
@@ -21,18 +26,29 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 @app.on_event("startup")
 async def startup_event():
     initialize_db()
+
 
 @app.get("/recherche")
 async def recherche(q: str):
     result = recherche_dans_la_base(q)
     return {'find': result}
 
+
 @app.get("/users/{userId}")
 async def create_user(userId: int):
     user = findUserById(userId)
     if user is None:
         raise HTTPException(status_code=404, detail="User not found")
-    return  user
+    return user
+
+
+@app.post("/create-users")
+async def create(user: User):
+    result = createUser(user)
+    if result is False:
+        raise HTTPException(status_code=401, detail="Unable to create user")
+    return result
--- backend/src/model/User.py
+++ backend/src/model/User.py
@@ -1,5 +1,24 @@
+from passlib.context import CryptContext
 from pydantic import BaseModel
 
+
 class User(BaseModel):
-    name: str
-    age: int
+    nom: str
+    prenom: str
+    email: str
+    mdp: str
+    telephone: str
+    description_profil: str
+    profession_id: int
+    sous_domaine_id: int
+    entreprise: int
+
+
+def get_password_hash(password):
+    pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+    return pwd_context.hash(password)
+
+
+def verify_password(plain_password, hashed_password):
+    pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+    return pwd_context.verify(plain_password, hashed_password)
