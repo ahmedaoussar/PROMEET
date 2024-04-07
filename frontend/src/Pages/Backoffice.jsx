b871edb9f66fb0e8dbb0efbc898bce2d10a52273
ahmed aoussar <ahmed.aoussar2000@gmail.com>
2024-04-07 13:27:35 +0200

    Backoffice Page VERSION FINAL

--- frontend/src/Pages/Backoffice.jsx
+++ frontend/src/Pages/Backoffice.jsx
@@ -1,9 +1,13 @@
 import Backoffice from '../Composants/BackOffice';
+import BarreNavigation from '../Composants/BarreNavigation';
+import Footer from '../Composants/Footer';
 
 const Artisans = () => {
   return (
     <div>
       <Backoffice />
+      <BarreNavigation/>
+      <Footer/>
     </div>
   );
 };
