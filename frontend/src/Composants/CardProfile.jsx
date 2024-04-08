790db574f516d988caf6e1e81071c9ca82dc8d0d
nicolas castex <nicolas.castex.pro@gmail.com>
2024-04-08 11:00:10 +0200

    uniformisation

--- frontend/src/Composants/CardProfile.jsx
+++ frontend/src/Composants/CardProfile.jsx
@@ -20,7 +20,7 @@ export const CardProfile = ({profil}) => {
     }
 
     return (
-        <Card className="mt-6 w-96">
+        <Card className="my-6 w-96">
             <CardHeader color="blue-gray" className="relative h-56">
                 <img
                     src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
