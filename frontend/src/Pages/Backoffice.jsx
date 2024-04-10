9a657c282e50fb67bad18957f61a81f6449b4645
ahmed aoussar <ahmed.aoussar2000@gmail.com>
2024-04-10 09:59:40 +0200

    backoffice

---
+++ frontend/src/Pages/Backoffice.jsx
@@ -0,0 +1,14 @@
+import { BarreNavigation } from "../Composants/BarreNavigation"
+import { Footer } from "../Composants/Footer"
+import { BackOffice } from "../Composants/BackOffice"
+
+
+export const Connexion = () => {
+    return (
+        <div>
+            <BackOffice></BackOffice>
+            <Footer></Footer>
+           <BarreNavigation></BarreNavigation>
+        </div>
+    )
+}
